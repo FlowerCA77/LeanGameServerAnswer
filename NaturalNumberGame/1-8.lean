07afba093c01db8d0a190eabd0f5e95f269fcdac
XhernoPhys CA77 <Ca538468142@outlook.com>
2024-07-29 21:36:48 +0800

    Update and rename NaturalNumberGame/1-8.lean to NaturalNumberGame/World1/1-8.lean

--- NaturalNumberGame/1-8.lean
+++
@@ -1,7 +0,0 @@
-nth_rewrite 2 [two_eq_succ_one] -- 只将第二个 `2 ` 改为 `succ 1` 。
-rw [add_succ]
-rw [one_eq_succ_zero]
-rw [add_succ, add_zero] -- 一次改写两个内容
-rw [← three_eq_succ_two] -- 将 `succ 2` 改为 `3`
-rw [← four_eq_succ_three] 
-rfl
